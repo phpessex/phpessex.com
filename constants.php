90c814b73a521848e629249921fc6bdfbab980ac
Andy Hunt <axhunt@arinc.com>
2017-02-07 09:45:19 +0000

    Adding Feb meetup and slack invite page

--- constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SUBDOMAIN','phpessex');
-define('TOKEN','xoxp-111289734689-111258952896-138529598214-c49f3538cf2b0870f9e1f2b040cbd813');
-define('SITEKEY', '6LdxZA4UAAAAAB8gPxAYpz15PGC3T5Eb1Rld5qXn');
-define('SECRET', '6LdxZA4UAAAAAAhxLKwCZHuhVl6pQVEIggU9Aww0');
-define('NOTE', 'You\'ve been invited to join the PHP Essex slack team!');
